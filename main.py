e057c9fd2747b148fd726afda6bd445a57f816c4
Ammar Hajeer <amarftw1@gmail.com>
2023-02-19 00:21:03 +0100

    imporvements, comments

--- main.py
+++ main.py
@@ -16,16 +16,27 @@ print("Please select your language,")
 for language in translations:
     print("{0.number}. | {0.localName} ({0.code})".format(language))
 
-language = translations[parse_input(max_length=len(translations)) - 1]
+choice = parse_input(max_length=len(translations))
+
+# Check in case user provides 0, so it doesn't end up being -1 (last element). Automatically pick English.
+language = translations[choice - 1 if choice else choice]
 path = parse_path(language)
+
 cls()
 songs = parse_songs(language)
 
 for c, (link, title) in enumerate(songs):
-    print(f'{c}/{len(songs)} {language.songDown} {colored(">>", "red")} "{title}"')
+    print(
+        f'{c}/{len(songs)} {language.songDown} {colored(">>", "red")} "{title}"',
+        end="\r",
+    )
 
     subprocess.run(
         f'yt-dlp --extract-audio --audio-format mp3 --quiet -o "{path}{parse_filename(title)}.mp3" {link}'
     )
 
+    print("-", end="\r")
+    input()
+
+cls()
 input(language.endText)
